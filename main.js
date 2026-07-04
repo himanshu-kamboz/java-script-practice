df4c5410659692d739f11a10cba72a95ac3769c9
Himanshu Kamboj <himanshukamboj0027@gmail.com>
2026-07-04 11:19:39 +0530

    number counter

--- main.js
+++
@@ -1,7 +0,0 @@
-let newNevBar = document.getElementById("nav-items")
-let btn = document.getElementById("hide");
-
-btn.addEventListener("click" , ()=>{
-    newNevBar.classList.toggle("active");
-    btn.classList.add("btn")
-})
